f24b60a7a25e6e647722b45b778499b0aedf4f8e
wtser <wtser@qq.com>
2015-02-09 11:33:53 +0800

    add 导出功能

--- js/options.js
+++ js/options.js
@@ -1,17 +1,64 @@
+var router = {
+    ""               : "officialContent",
+    "officialContent": "officialContent",
+    "diyContent"     : "diyContent",
+    "importExport"   : "importExport"
+};
+var opt = {
+    init           : function () {
+        opt.render();
+        window.onhashchange = function () {
+            opt.render();
+        }
+    },
+    render         : function () {
+        var hash = location.hash.replace("#", "") || "officialContent";
+        $(".main").addClass("am-hide");
+        $("#" + hash).removeClass("am-hide");
+        $(".am-nav>li").removeClass("am-active");
+        $(".am-nav a[href='#" + hash + "']").closest("li").addClass("am-active");
+
+        opt[router[hash]]();
+    },
+    officialContent: function () {
+
+    },
+    diyContent     : function () {
+        console.log("diyContent")
+    },
+    importExport   : function () {
+
+        $(".export").on("click",function(){
+            var data = localStorage.getItem("diyContent");
+
+            var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
+            saveAs(blob, "hotenBackup.json");
+        });
+
+        $(".import").on("change",function(){
+            var file = $(this).val();
+            console.log(file)
+        })
+    }
+};
+
+opt.init();
+
+
 var data = JSON.parse(localStorage.getItem("diyContent")) || [];
 
 function renderDiyTable() {
-    var html = _.reduce(data,function(memo,d,index){
-        var checkState = d.isShow?"checked":"";
-        return memo+"<tr data-k='"+index+"'>" +
-            '<td><input name="isShow" type="checkbox" '+ checkState +' ></td>' +
-            '<td><img style="width: 16px;" src="'+ d.icon+'"> <input name="icon" type="text" value="'+ d.icon+'"></td>' +
-            '<td><input name="name" type="text" value="'+ d.name+'"></td>' +
-            '<td><input name="url" type="text" value="'+ d.url+'"></td>' +
-            '<td><input name="selector" type="text" value="'+ d.selector+'"></td>' +
+    var html = _.reduce(data, function (memo, d, index) {
+        var checkState = d.isShow ? "checked" : "";
+        return memo + "<tr data-k='" + index + "'>" +
+            '<td><input name="isShow" type="checkbox" ' + checkState + ' ></td>' +
+            '<td><img style="width: 16px;" src="' + d.icon + '"> <input name="icon" type="text" value="' + d.icon + '"></td>' +
+            '<td><input name="name" type="text" value="' + d.name + '"></td>' +
+            '<td><input name="url" type="text" value="' + d.url + '"></td>' +
+            '<td><input name="selector" type="text" value="' + d.selector + '"></td>' +
             '<td><button class="del">删</button></td>' +
             "</tr>";
-    },"");
+    }, "");
     $(".am-table>tbody").append(html);
 }
 
@@ -20,39 +67,39 @@ function addContent() {
 }
 
 $("#add").on("click", function () {
-    var _tr=$(this).closest("tr");
+    var _tr = $(this).closest("tr");
     var newContent = {
         isShow  : _tr.find("[name=isShow]").is(":checked"),
         icon    : _tr.find("[name=icon]").val(),
-        name   : _tr.find("[name=name]").val(),
+        name    : _tr.find("[name=name]").val(),
         url     : _tr.find("[name=url]").val(),
         selector: _tr.find("[name=selector]").val()
     }
     data.push(newContent);
-    localStorage.setItem("diyContent",JSON.stringify(data));
+    localStorage.setItem("diyContent", JSON.stringify(data));
     location.reload();
 });
 
-$(".am-table").on("click",".del",function(){
-    var _tr=$(this).closest("tr");
+$(".am-table").on("click", ".del", function () {
+    var _tr = $(this).closest("tr");
     var key = _tr.attr("data-k");
-    if(confirm("确认删除？")){
-        data.splice(key,1);
-        localStorage.setItem("diyContent",JSON.stringify(data));
+    if (confirm("确认删除？")) {
+        data.splice(key, 1);
+        localStorage.setItem("diyContent", JSON.stringify(data));
         _tr.remove();
     }
 });
 
-$(".am-table").on("change","input",function(){
-    var _tr=$(this).closest("tr");
+$(".am-table").on("change", "input", function () {
+    var _tr = $(this).closest("tr");
     var key = _tr.attr("data-k");
     var name = $(this).attr("name");
     var value = $(this).val();
-    if($(this).attr("type")=="checkbox"){
-        value=$(this).is(":checked");
+    if ($(this).attr("type") == "checkbox") {
+        value = $(this).is(":checked");
     }
-    data[key][name]=value;
-    localStorage.setItem("diyContent",JSON.stringify(data));
+    data[key][name] = value;
+    localStorage.setItem("diyContent", JSON.stringify(data));
 
 });
 
